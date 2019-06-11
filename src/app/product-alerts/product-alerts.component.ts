0a239947f62df69c78d4bece7567fcf4564d4286
shuuns <shuuns@gmail.com>
2019-06-11 10:59:50 +0800

    4-2. add import

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
