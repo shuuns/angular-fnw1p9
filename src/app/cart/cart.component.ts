1de41a6054770da34e5305c975bfd48c36aec994
shuuns <shuuns@gmail.com>
2019-06-12 17:26:31 +0800

    Managing Data-3-2-2. import CartService to CartComponent

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent implements OnInit {
 
-  constructor() { }
+export class CartComponent {
 
-  ngOnInit() {
-  }
-
+  constructor(
+    private cartService: CartService
+  ) { }
 }
